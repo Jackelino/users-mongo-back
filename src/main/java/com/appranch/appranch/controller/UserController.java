d707854cc3970adf98530f30850bdd15633a2937
jack24981 <jose.fernandez@mifel.com.mx>
2023-12-14 19:16:31 -0600

    feat: add return error of inputs

--- src/main/java/com/appranch/appranch/controller/UserController.java
+++ src/main/java/com/appranch/appranch/controller/UserController.java
@@ -3,6 +3,7 @@ package com.appranch.appranch.controller;
 import com.appranch.appranch.dao.UserInterface;
 import com.appranch.appranch.models.User;
 import com.appranch.appranch.requests.UserRequest;
+import com.appranch.appranch.utils.ErrorInput;
 import jakarta.validation.Valid;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -27,7 +28,11 @@ public class UserController {
 
     @PostMapping
     public ResponseEntity<?> save(@Valid @RequestBody UserRequest userRequest, BindingResult result) {
+        ErrorInput errorInput = new ErrorInput();
+        ResponseEntity<?> errors = errorInput.getResponseEntity(result);
+        if (errors != null) return errors;
         User user = this.userInterface.save(userRequest);
         return new ResponseEntity<User>(user, HttpStatus.CREATED);
     }
+
 }
---
+++ src/main/java/com/appranch/appranch/utils/ErrorInput.java
@@ -0,0 +1,22 @@
+package com.appranch.appranch.utils;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.FieldError;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class ErrorInput {
+    public ResponseEntity<?> getResponseEntity(BindingResult result) {
+        if (result.hasErrors()) {
+            Map<String, String> errors = new HashMap<>();
+            for (FieldError error : result.getFieldErrors()) {
+                errors.put(error.getField(), error.getDefaultMessage());
+            }
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(errors);
+        }
+        return null;
+    }
+}
